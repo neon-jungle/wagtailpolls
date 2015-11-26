963259d309c2f19f6d556afbac9b9b897d0fd1c6
Liam Brenner <liam.brenner@gmail.com>
2015-11-27 10:36:24 +1100

    Removed useless files, created template for deleting polls, reset migrations

--- wagtailpolls/migrations/0001_initial.py
+++ wagtailpolls/migrations/0001_initial.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.db import migrations, models
 import modelcluster.fields
 import django.utils.timezone
+import wagtail.wagtailsearch.index
 
 
 class Migration(migrations.Migration):
@@ -22,6 +23,7 @@ class Migration(migrations.Migration):
             options={
                 'abstract': False,
             },
+            bases=(models.Model, wagtail.wagtailsearch.index.Indexed),
         ),
         migrations.CreateModel(
             name='Question',
@@ -29,6 +31,18 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', auto_created=True, primary_key=True, serialize=False)),
                 ('question', models.CharField(max_length=128)),
                 ('poll', modelcluster.fields.ParentalKey(to='wagtailpolls.Poll', related_name='questions')),
+            ],
+            options={
+                'abstract': False,
+            },
+        ),
+        migrations.CreateModel(
+            name='Vote',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', auto_created=True, primary_key=True, serialize=False)),
+                ('ip', models.GenericIPAddressField()),
+                ('time', models.DateTimeField(auto_now_add=True)),
+                ('question', modelcluster.fields.ParentalKey(to='wagtailpolls.Question', related_name='votes')),
             ],
         ),
     ]
--- wagtailpolls/views/vote.py
+++ wagtailpolls/views/vote.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import permission_required
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404
 from django.http import HttpResponse, JsonResponse
 from ..models import Poll, Vote
 from ..forms import VoteForm
