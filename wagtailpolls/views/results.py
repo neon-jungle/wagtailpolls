44eea24a9e773ceba867b93cf722d7861f3db638
Liam Brenner <liam.brenner@gmail.com>
2015-11-27 10:30:37 +1100

    Added results page

--- wagtailpolls/views/results.py
+++ wagtailpolls/views/results.py
@@ -4,10 +4,13 @@ from django.shortcuts import render, get_object_or_404
 
 @permission_required('wagtailadmin.access_admin')
 def results(request, poll_pk):
-    from ..models import Poll
+    from ..models import Poll, Vote
 
     poll = get_object_or_404(Poll, pk=poll_pk)
+    votes = Vote.objects.filter(question__poll=poll)
+    total_votes = votes.count()
 
     return render(request, 'wagtailpolls/poll_results.html', {
         'poll': poll,
+        'total_votes': total_votes,
     })
